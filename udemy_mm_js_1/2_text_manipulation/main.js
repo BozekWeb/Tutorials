766a37563d51e89f3920e5c710c40312aaa359ac
Jakub Bożek <praca_github@bozek.website>
2022-05-17 17:16:35 +0200

    Change readme.md and add 2 project

---
+++ udemy_mm_js_1/2_text_manipulation/main.js
@@ -0,0 +1,31 @@
+const sizeUpBtn = document.querySelector('.sizeUp');
+const sizeDownBtn = document.querySelector('.sizeDown');
+const colorBtn = document.querySelector('.color');
+const text = document.querySelector('p');
+
+fontSize = 36;
+
+const sizeUp = () => {
+    fontSize += 4;
+    text.style.fontSize = fontSize + "px";
+}
+
+const sizeDown = () => {
+    if(fontSize <= 16) return;
+
+    fontSize -=4;
+    text.style.fontSize = fontSize + "px";
+}
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    text.style.color = `rgb(${r}, ${g}, ${b})`
+}
+
+
+sizeUpBtn.addEventListener("click", sizeUp);
+sizeDownBtn.addEventListener("click", sizeDown);
+colorBtn.addEventListener("click", randomColor);
--- udemy_mm_js_1/README.md
+++ udemy_mm_js_1/README.md
@@ -1,2 +1,15 @@
-# Tutorials 1
-A repository containing projects made as part of courses from various online platforms
+# "[2022] JavaScript - Jedyny Kurs, Którego Potrzebujesz"
+A extensive JavaScript course, covering the basics of the language, as well as more advanced topics such as: DOM manipulation, objects and using the API.
+
+As part of the course, I have completed 8 projects*:
+1. Image manipulation
+2. Text manipulation
+3. RegExp and password validation
+4. Celsius to Fahrenheit converter
+5. Hamburger menu
+6. Days and trivia
+7. ToDo List
+8. Weather application
+
+Some of the projects were done 1:1 according to the tutorial, and partly I added some smaller features as part of the self exercise.
+
