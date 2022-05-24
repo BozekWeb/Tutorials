9b85ae7f3cd6730b605169cdddaf82889510da46
Jakub Bożek <praca_github@bozek.website>
2022-05-24 17:29:16 +0200

    Project #1 - Add additional RegExp solution

--- udemy_mm_js_2/1_text_finder/main.js
+++ udemy_mm_js_2/1_text_finder/main.js
@@ -1,16 +1,29 @@
 const search = document.querySelector('.search')
 const li = document.querySelectorAll('li')
 
-const searchEngine = e => {
-	const text = e.target.value.toLowerCase()
+// const searchEngine = e => {
+// 	const text = e.target.value.toLowerCase()
 
+// 	li.forEach(el => {
+// 		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+// 			el.style.display = 'block'
+// 		} else {
+// 			el.style.display = 'none'
+// 		}
+// 	})
+// }
+
+const searchEngineRegExp = () => {
 	li.forEach(el => {
-		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
-			el.style.display = 'block'
-		} else {
+		const match = new RegExp(search.value, 'i').test(el.textContent)
+
+		if (!match) {
 			el.style.display = 'none'
+		} else {
+			el.style.display = 'block'
 		}
 	})
 }
 
-search.addEventListener('keyup', searchEngine)
+//search.addEventListener('keyup', searchEngine)
+search.addEventListener('keyup', searchEngineRegExp)
