7fc774343678804c72beea9571b39f231d83d329
Jakub Bożek <praca_github@bozek.website>
2022-05-26 21:28:29 +0200

    Project #4 - Complete

--- udemy_mm_js_2/4_magic_ball/main.js
+++ udemy_mm_js_2/4_magic_ball/main.js
@@ -1,101 +1,35 @@
-const username = document.querySelector('#username')
-const password1 = document.querySelector('#password')
-const password2 = document.querySelector('#password2')
-const email = document.querySelector('#email')
-const allInput = document.querySelectorAll('input')
+const ball = document.querySelector('.ball-img')
+const input = document.querySelector('input')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
 
-const sendBtn = document.querySelector('.send')
-const clearBtn = document.querySelector('.clear')
+const answers = ['Nie', 'Tak', 'Tego nie wiem', 'Czy napewno chcesz to wiedzieć?', 'To zależy..']
 
-const popup = document.querySelector('.popup')
-const closeBtn = document.querySelector('.close')
-
-const showError = (input, msg) => {
-	const formBox = input.parentElement
-	const errorMsg = formBox.querySelector('.error-text')
-
-	formBox.classList.add('error')
-	errorMsg.textContent = msg
+const shakeBall = () => {
+	ball.classList.add('shake-animaton')
 }
 
-const clearError = input => {
-	input.parentElement.classList.remove('error')
-}
+const checkInput = () => {
+	ball.classList.remove('shake-animaton')
 
-const checkForm = input => {
-	input.forEach(el => {
-		if (el.value === '') {
-			showError(el, el.placeholder)
-		} else {
-			clearError(el)
-		}
-	})
-}
-
-const checkLength = (input, min) => {
-	if (input.value.length < min) {
-		showError(
-			input,
-			`${input.previousElementSibling.innerText.replace(':', '')} posiada za mało znaków, minimum to: ${min}`
-		)
-	}
-}
-
-const checkPassword = (pass1, pass2) => {
-	if (pass1.value !== pass2.value) {
-		showError(pass2, `Podane hasła nie są identyczne`)
-	}
-}
-
-const checkMail = email => {
-	const val =
-		/^(([^<>()\[\]\\.,;:s@"]+(\.[^<>()\[\]\\.,;:s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-	if (val.test(email.value)) {
-		clearError(email)
+	if (input.value !== '' && input.value.slice(-1) === '?') {
+		generateAnswer()
+		error.textContent = ''
+	} else if (input.value !== '' && input.value.slice(-1) !== '?') {
+		answer.textContent = ''
+		error.textContent = "Umieść '?' na końcu pytania"
 	} else {
-		showError(email, 'E-mail jest niepoprawny')
+		answer.textContent = ''
+		error.textContent = 'Wpisz pytanie!'
 	}
 }
 
-const sendForm = () => {
-	const err = 0
-
-	allInput.forEach(el => {
-		if (el.previousElementSibling.classList.contains('error')) err++
-	})
-
-	if (err === 0) {
-		popup.classList.add('show-popup')
-	}
+const generateAnswer = () => {
+	const number = Math.floor(Math.random() * answers.length)
+	answer.innerHTML = `<span>Odpowiedź:</span> ${answers[number]}`
 }
 
-sendBtn.addEventListener('click', e => {
-	e.preventDefault()
-	checkForm(allInput)
-	checkLength(username, 8)
-	checkLength(password1, 10)
-	checkPassword(password1, password2)
-	checkMail(email)
+const showAnswer = () => {}
 
-	sendForm()
-})
-
-clearBtn.addEventListener('click', e => {
-	e.preventDefault()
-	allInput.forEach(el => {
-		el.value = ''
-		clearError(el)
-	})
-})
-
-closeBtn.addEventListener('click', e => {
-	e.preventDefault()
-
-	popup.classList.remove('show-popup')
-
-	allInput.forEach(el => {
-		el.value = ''
-		clearError(el)
-	})
-})
+ball.addEventListener('click', shakeBall)
+ball.addEventListener('animationend', checkInput)
