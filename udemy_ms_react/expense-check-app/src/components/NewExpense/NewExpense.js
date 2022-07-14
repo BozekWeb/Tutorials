6ae64993bc49418dd9e7a5a1be34bc0c1b4301b2
Jakub Bożek <praca_github@bozek.website>
2022-07-14 22:01:20 +0200

    Add new components, learn about state, lifting state up

---
+++ udemy_ms_react/expense-check-app/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import React from 'react' //not needed
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css'
+
+const NewExpense = props => {
+	const onSaveExpenseDataHandler = enterExpenseData => {
+		const expenseData = {
+			...enterExpenseData,
+			id: Math.random().toString,
+		}
+		props.onAddExpense(expenseData)
+	}
+
+	return (
+		<div className='new-expense'>
+			<ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+		</div>
+	)
+}
+
+export default NewExpense
