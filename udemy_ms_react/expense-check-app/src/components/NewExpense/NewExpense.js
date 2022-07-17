198893c75fc1f723f3f865e46b13f73e41987288
Jakub Bożek <praca_github@bozek.website>
2022-07-17 12:03:13 +0200

    Add filter feature

--- udemy_ms_react/expense-check-app/src/components/NewExpense/NewExpense.js
+++ udemy_ms_react/expense-check-app/src/components/NewExpense/NewExpense.js
@@ -1,19 +1,31 @@
-import React from 'react' //not needed
+import React, { useState } from 'react' //not needed
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = props => {
+	const [isEditing, setIsEditing] = useState(false)
+
 	const onSaveExpenseDataHandler = enterExpenseData => {
 		const expenseData = {
 			...enterExpenseData,
 			id: Math.random().toString,
 		}
 		props.onAddExpense(expenseData)
+		setIsEditing(false)
+	}
+
+	const startEditingHandler = () => {
+		setIsEditing(true)
+	}
+
+	const stopEditingHandler = () => {
+		setIsEditing(false)
 	}
 
 	return (
 		<div className='new-expense'>
-			<ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+			{!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+			{isEditing && <ExpenseForm onCancel={stopEditingHandler} onSaveExpenseData={onSaveExpenseDataHandler} />}
 		</div>
 	)
 }
